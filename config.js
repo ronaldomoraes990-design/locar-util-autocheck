03afd9249d618e28bfde740bbf079fe9854714b5
ronaldomoraes990-design <ronaldo.moraes990@gmail.com>
2026-09-15 23:02:56 -0300

    corrige chave publica do Supabase no login

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.AUTOCHECK_CONFIG = {
   supabaseUrl: "https://qfiunsqjkkxuetmyzmdw.supabase.co",
-  supabaseAnonKey: "sb_publishable_dlBACJeZVFw7ZUcGGRfDPA_8n8_ivRW",
+  supabaseAnonKey: "sb_publishable_dlBACJeZVFwZ7UcGGRfDPA_8n8_ivRW",
   bucket: "checklists"
 };
 
